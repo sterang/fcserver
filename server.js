33e9a0ef2ca0103963d01813895272eb02c3cb7d
sterang <hamil@unicauca.edu.co>
2019-10-15 21:27:07 -0500

    Server with upload files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require('express');
+const cors = require('cors');
 const activitiesRoutes = require('./activities/activities_routes');
 const estudianteRoutes = require('./authE/authE_routes');
 const docenteRoutes = require('./authD/authD_routes');
@@ -10,14 +11,29 @@ const DB = require('./config/db');
 DB();
 
 const app = express();
+app.use(cors());
 const router = express.Router();
 
 const bodyParser = require('body-parser');
+const multipart = require('connect-multiparty');  
+
+const multipartMiddleware = multipart({  
+    uploadDir: './public/images'
+});
+
 const bodyParserJSON = bodyParser.json();
 const bodyParserURLEncode = bodyParser.urlencoded({extended:true});
 
 app.use(bodyParserJSON);
 app.use(bodyParserURLEncode);
+
+app.post('/subir', multipartMiddleware, (req, res, next) => {  
+    console.log(req.files);
+    res.json({
+        'message': `File uploaded succesfully. Nombre: ${req.query}`
+    });
+});
+
 
 app.use('/api', router);
 activitiesRoutes(router);
@@ -33,4 +49,7 @@ router.get('/',(req, res)=>{
 
 app.use(router);
 app.use(express.static('public'));
+//Newww for Upload
+
+
 app.listen(3000,()=> console.log(`Server running port 3000`));
